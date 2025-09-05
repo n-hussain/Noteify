529a17085d28e563a8538ff867b484783cccd7e3
Nurul Hussain <86896592+n-hussain@users.noreply.github.com>
2025-09-04 20:39:24 -0400

    changed local db call

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:trjtftfutd@localhost:5432/noteify_db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
