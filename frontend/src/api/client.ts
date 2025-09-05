9abba4c2b83563a809f529680a44d2368b388e99
Nurul Hussain <86896592+n-hussain@users.noreply.github.com>
2025-09-04 20:57:10 -0400

    connected frontend to non-local db

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://noteify-3c3u.onrender.com",
 });
 
 client.interceptors.request.use((config) => {
